c718b6f903192ad0ff74f39b84665200adf50b24
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-12-21 21:04:27 +0100

    psw generator lowercase/uppercase si

--- password-generator.py
+++ password-generator.py
@@ -1,1 +1,12 @@
+import sys
+
 password = []
+
+password_length = int(input("Provide password length (min 8 signs): "))
+
+if password_length < 8:
+    print("Password is too short. Password should have min. 8 signs")
+    sys.exit(0)
+
+lowercase_letters = input(input("How many lowercase :"))
+uppercase_letters = input(input("How many uppercase :"))
